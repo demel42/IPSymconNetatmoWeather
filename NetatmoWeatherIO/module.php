98717a0cda6f84e03ce5a790616c236d5b9f9882
demel42 <demel42@web.de>
2018-05-19 11:01:18 +0200

    Fehlermeldung verbessert

--- NetatmoWeatherIO/module.php
+++ NetatmoWeatherIO/module.php
@@ -230,8 +230,8 @@ class NetatmoWeatherIO extends IPSModule
         }
 
         if ($statuscode) {
-            echo " => statuscode=$statuscode, err=$err";
-            $this->SendDebug(__FUNCTION__, $err, 0);
+			echo "url=$url => statuscode=$statuscode, err=$err";
+			$this->SendDebug(__FUNCTION__, ' => statuscode=' . $statuscode . ', err=' . $err, 0);
             $this->SetStatus($statuscode);
         }
 
