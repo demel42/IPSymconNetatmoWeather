126c127dd98ff94764ca7ad08b2f726a77344c42
demel42 <demel42@web.de>
2018-05-29 16:27:40 +0200

    LogMessage() ist protected

--- libs/common.php
+++ libs/common.php
@@ -92,7 +92,7 @@ trait NetatmoWeatherCommon
         return 'text/plain';
     }
 
-    private function LogMessage($Message, $Severity)
+    protected function LogMessage($Message, $Severity)
     {
         if (IPS_GetKernelVersion() >= 5) {
             switch ($Severity) {
